b266d397e27253f0b3bd1478db6295ed6651e0e3
Vivaan Singh <vsingh7205@outlook.com>
2025-08-17 09:53:04 -0400

    adding project

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/astropcs-website/', // Adjust this to your GitHub repo name
 }));
